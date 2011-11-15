d388d1c832c1ea2ee4b85c2a8a34afc232c61143
Charlie Tian <charlie.tian@gmail.com>
2011-11-15 00:55:19 -0500

    added shopping cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def current_card
+    Cart.find(session[:cart_id])
+  rescue
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Depot::Application.routes.draw do
 
+  resources :line_items
+
+  resources :carts
+
   root to: 'store#index', as:'store'
   get "store/index"
 
