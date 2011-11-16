921a032b714d6514ade16527f6ba543ff2998c23
Charlie Tian <charlie.tian@gmail.com>
2011-11-15 20:47:52 -0500

    added store visit counter

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -40,6 +40,8 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
+    session[:counter] = 0
+
     @cart = current_card
     product = Product.find(params[:product_id])
     @line_item = @cart.line_items.build(product: product)
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,14 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @counter = count
   end
 
+  private 
+
+  def count
+    session[:counter] ||= 0
+    session[:counter] += 1
+    session[:counter]
+  end
 end
